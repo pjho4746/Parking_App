de63f54becf8f8d9ffba7fe8f2d198ccef79be62
hayannn <dlgkdis801@naver.com>
2023-11-23 13:51:16 +0900

    폴더 변경

--- server/src/main/java/com/humax/parking/ParkingApplication.java
+++ server/src/main/java/com/humax/parking/ParkingApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ParkingApplication {
+public class ParkingApplication { //추가
 
 	public static void main(String[] args) {
 		SpringApplication.run(ParkingApplication.class, args);
